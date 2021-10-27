cf7148127f366921a85d4984f020a9e3fc5c77d5
amolu <amolujagare@gmail.com>
2021-10-27 22:04:21 +0530

    Initial commit

---
+++ src/SeleniumDemo2.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+public class SeleniumDemo2 {
+
+    public static void main(String[] args) {
+
+      /*  System.setProperty("webdriver.gecko.driver","Drivers/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+*/
+
+       /* System.setProperty("webdriver.edge.driver","Drivers/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();*/
+
+        System.setProperty("webdriver.opera.driver","Drivers/operadriver.exe");
+        WebDriver driver = new OperaDriver();
+
+        driver.manage().window().maximize();
+        driver.get("http://gmail.com");
+    }
+}
